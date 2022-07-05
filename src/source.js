ef757b7aff7711120182b06f4edbf13f22874011
bertustenhoor <bertustenhoor71@gmail.com>
2022-07-05 16:01:34 +0200

    The next day

---
+++ src/source.js
@@ -0,0 +1,22 @@
+export const robots = [
+    {
+        id: 'John Doe',
+        name: 'Johannes Doe',
+        email: 'j.doe@gmail.com',
+    },
+    {
+        id: 'Ida Hoe',
+        name: 'Ida Whatshername',
+        email: 'I.Hoe@gmail.com',
+    },
+    {
+        id: 'Sammie',
+        name: 'Bertus ten Hoor',
+        email: 'bertustenhoor71@gmail.com',
+    },
+    {
+        id: `Sammie's girl`,
+        name: 'Titia ten Hoor',
+        email: 'titiatenhoor@gmail.com',
+    },
+]
